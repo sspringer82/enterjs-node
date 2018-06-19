18a45fef2a77be6d5c351604622a0bc1cc9d8446
sspringer82 <info@sebastian-springer.com>
2018-06-19 15:50:13 +0200

    passport part 2

--- my-enterprise-app/index.js
+++ my-enterprise-app/index.js
@@ -6,12 +6,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const todoRouter = require('./todo');
 const logger = require('./logger');
+const auth = require('./auth');
 
 const app = express();
 
 logger(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+auth(app);
 
 app.use(express.static('public'));
 
